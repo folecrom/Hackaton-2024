51c3cede2b6376f53994aee69c8a92b964ebf9c4
Xelame <m2afkeb9@duck.com>
2024-10-15 10:58:15 +0200

    Add gitignore

--- wall-of-blame/src/extension.ts
+++ wall-of-blame/src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('extension.showGitContributors', () => {
+    // Récupérer le dossier racine du projet ouvert
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders) {
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      
+      // Commande Git pour obtenir les contributeurs
+      const gitCommand = 'git shortlog -s -n';
+      
+      // Exécuter la commande Git dans le répertoire du projet
+      exec(gitCommand, { cwd: rootPath }, (error, stdout, stderr) => {
+        if (error) {
+          vscode.window.showErrorMessage(`Erreur lors de l'exécution de la commande Git: ${error.message}`);
+          return;
+        }
+        
+        if (stderr) {
+          vscode.window.showErrorMessage(`Erreur: ${stderr}`);
+          return;
+        }
+        
+        // Afficher les contributeurs dans VS Code
+        vscode.window.showInformationMessage(`Contributeurs du projet :\n${stdout}`);
+      });
+    } else {
+      vscode.window.showErrorMessage('Aucun projet ouvert.');
+    }
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wall-of-blame" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('wall-of-blame.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Wall of Blame!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+  console.log(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
